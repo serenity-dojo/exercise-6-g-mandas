08764441cf10c7a4a25384e807103441f77be86b
Gokhan Mandas <gokhan@mandas.net>
2023-02-11 15:52:19 +0000

    answers to exercise-6

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -2,6 +2,23 @@ package com.serenitydojo.model;
 
 public class Feeder {
     public String feeds(String animal, boolean isPremium) {
+        if (animal.equals("Cat")) {
+            if (isPremium) {
+                return "Salmon";
+            } else {
+                return "Tuna";
+            }
+
+        } else if (animal.equals("Dog")) {
+            if (isPremium) {
+                return "Wet Food";
+            } else {
+                return "Dog Food";
+            }
+        } else if (animal.equals("Hamster")) {
+            return (isPremium) ? "Nuts" : "Cabbage";
+        }
+
         return "Food";
     }
 }
